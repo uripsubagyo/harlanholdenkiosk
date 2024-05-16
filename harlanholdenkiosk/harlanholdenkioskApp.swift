96f3b412da5375efe808a2d478f80f8cfba3ed44
uripsubagyo <uripsubagy@gmail.com>
2024-05-16 08:22:19 +0700

    init project

---
+++ harlanholdenkiosk/harlanholdenkioskApp.swift
@@ -0,0 +1,17 @@
+//
+//  harlanholdenkioskApp.swift
+//  harlanholdenkiosk
+//
+//  Created by I Made Urip Subagyo on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct harlanholdenkioskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
